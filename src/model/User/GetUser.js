5902f806ae34536962c6fb91e8b3a6d56898007e
maithevien9 <maithevien9@gmail.com>
2021-08-15 03:24:30 +0700

    add

--- src/model/User/GetUser.js
+++ src/model/User/GetUser.js
@@ -1,7 +1,7 @@
 var dataString = "";
 module.exports = function (db, callback) {
   let sql = `
-SELECT user.NAME, user.id, faculty.description,CASE WHEN user.sex = 1 THEN 'Nam' WHEN user.sex = 0 THEN 'Nữ' END as Gender, user.address,user.age from user inner join faculty on user.idFaculty = faculty.id  where user.idRole = '2'`;
+SELECT user.NAME, user.id, faculty.description,CASE WHEN user.sex = 1 THEN 'Nam' WHEN user.sex = 0 THEN 'Nữ' END as Gender, user.address,user.age from user inner join faculty on user.idFaculty = faculty.id  where user.idRole = '1'`;
   db.query(sql, function (err, results, fields) {
     if (err) {
       throw err;
